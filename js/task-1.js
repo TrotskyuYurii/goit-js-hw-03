66579d8fb74320c9d84fb9d1e1c4afb7b56bed8b
TrotskyuYurii <tro2001@gmail.com>
2023-11-11 19:17:11 +0200

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -18,8 +18,6 @@ function slugify(title){
 
 
 
-
-
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
