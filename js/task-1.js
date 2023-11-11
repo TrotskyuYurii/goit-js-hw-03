9de9c726e13b46f42391607e8b55ca95a7e11bd6
TrotskyuYurii <tro2001@gmail.com>
2023-11-11 18:55:34 +0200

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,26 @@
+function slugify(title){
+
+    let resslug =   '';
+    let tempargs = Array.from(title.toLowerCase());
+
+    for (const arg of tempargs) {
+       
+        if (arg==' '){
+            resslug =   resslug+'-';    
+        } else {
+        resslug=resslug+arg;
+      }
+
+    }
+
+    return resslug;
+}
+
+
+
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
